804b5251b0abe75744f46ec924153b03b27bfd7c
Huu Bang Doan <chocotochan@gmail.com>
2026-09-14 11:07:01 +0700

    feat: add spring-boot framework

---
+++ Pacman/Pacman/src/test/java/Iuer/example/Pacman/PacmanApplicationTests.java
@@ -0,0 +1,13 @@
+package Iuer.example.Pacman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
